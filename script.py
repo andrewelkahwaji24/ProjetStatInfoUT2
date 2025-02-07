3a5a04285979fbd356dd402142112d81594c0f50
Andrew El Kahwaji <158772084+andrewelkahwaji24@users.noreply.github.com>
2025-02-07 19:47:03 +0100

    Updated libraries and created new functions for better management

--- script.py
+++ script.py
@@ -1,19 +1,21 @@
 import sqlite3 , numpy as np , os , random, csv
+from scipy import stats
+import statsmodels.api as sm
+from datetime import datetime
+
 
 #creation d'une base de donnée
-
+#creation d'une connexion avec la base de donnees
 def connecterdb(database="data.db"):
     connexion=sqlite3.connect(database)
     curs=connexion.cursor()
     return connexion, curs
 
-connecterdb()
-
 def creer_table_incendies():
-    connexion , curs = connecterdb()
+    connexion, curs = connecterdb()
     curs.execute(
         '''
-        CREATE TABLE  incendies(
+        CREATE TABLE IF NOT EXISTS incendies(
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             commune TEXT NOT NULL,
             code_INSEE TEXT NOT NULL,
@@ -23,14 +25,23 @@ def creer_table_incendies():
             jour INTEGER NOT NULL,
             heure INTEGER NOT NULL,
             nature_inc_prim TEXT NOT NULL,
-            nature_inc_sec TEXT
-            )
+            nature_inc_sec TEXT NOT NULL
+        )
         '''
     )
-    #confirme la creation
+    # Confirme la création
     connexion.commit()
-    #fermer le courser et la connexion
+    # Fermer le curseur et la connexion
     curs.close()
     connexion.close()
 
-creer_table_incendies()
+#C'est une fonction generale pour pouvoir editer des trucs , creer des tables , inserer ou modifier des donees
+def operations_non_observatrices():
+    creer_table_incendies()
+
+#La fonction main est utilise pour pouvoir lance un ensemble de fonctions
+#C'est un programme generale neccessaire pour l'execution et on peut l'executer autant de fois qu'on veut
+def main():
+    connecterdb()
+
+main()

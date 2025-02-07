b22a8f5c88797cc2cdf1858a1f51189d6389906c
Andrew El Kahwaji <158772084+andrewelkahwaji24@users.noreply.github.com>
2025-02-07 16:01:03 +0100

    Update libraries

--- script.py
+++ script.py
@@ -8,3 +8,24 @@ def connecterdb(database="data.db"):
     return connexion, curs
 
 connecterdb()
+
+def creer_table_incendies():
+    connexion , curs = connecterdb()
+    curs.execute(
+        '''
+        CREATE TABLE  incendies(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            commune TEXT NOT NULL,
+            code_INSEE TEXT NOT NULL,
+            surface_parcourue_m2 INTEGER NOT NULL,
+            annee INTEGER NOT NULL,
+            mois TEXT NOT NULL,
+            jour INTEGER NOT NULL,
+            heure INTEGER NOT NULL,
+            nature_inc_prim TEXT NOT NULL,
+            nature_inc_sec TEXT
+            )
+        '''
+    )
+
+creer_table_incendies()
